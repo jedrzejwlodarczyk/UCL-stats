d1f0f893993f8a28558f2c1cb9368b3461f28c94
jedrzejwlodarczyk <jedrzejwlodarczyk@gmail.com>
2026-03-18 18:01:20 +0100

    Fix AttackingData entity

--- backend/src/main/java/pl/Wlodarczyk/UCL_Stats/model/AttackingData.java
+++ backend/src/main/java/pl/Wlodarczyk/UCL_Stats/model/AttackingData.java
@@ -2,13 +2,21 @@ package pl.Wlodarczyk.UCL_Stats.model;
 
 import jakarta.persistence.*;
 import jakarta.persistence.criteria.CriteriaBuilder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import pl.Wlodarczyk.UCL_Stats.repository.PlayerRepository;
 
 @Entity
 @Table(name = "attacking_data")
+@Getter @Setter @NoArgsConstructor
 public class AttackingData {
     @Id
+    @Column(name = "id_player")
+    private Long idPlayer;
+
     @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
     @JoinColumn(name = "id_player", nullable = false)
     private Player player;
 
